2684ff6e68dac4a8c47307ee7deccceef508e001
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-02-09 21:35:12 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000106169_InocentiusNoelPrasetya.week01/GradingSystem.main.kts
@@ -0,0 +1,8 @@
+package oop_00000106169_InocentiusNoelPrasetya.week01
+
+fun main(args: Array<String>) {
+    var studentName: String = "John"
+    var score: Int = 85
+    println("Student: " + studentName + ", Score: " + score)
+}
+
