d8a522331fb37c053b227f4dfdce808e90da99b9
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-02-24 20:09:28 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000106169_InocentiusNoelPrasetya/week03/Employee.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000106169_InocentiusNoelPrasetya.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE INI SALAH & BERBAHAYA!
+            this.salary = value  // Ini akan recursive!
+        }
 }
