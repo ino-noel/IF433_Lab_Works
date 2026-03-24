8e2a9e88b0b42bed8a154c0d28fcdecfd2c93266
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-03-24 23:38:30 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000106169_InocentiusNoelPrasetya/week06/Main.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week06/Main.kt
@@ -1,9 +1,8 @@
 package oop_00000106169_InocentiusNoelPrasetya.week06
 
-// Fungsi ini Decoupled! Tidak peduli kelas aslinya apa.
 fun processCheckout(method: PaymentMethod, amount: Double) {
     println("-> Memulai checkout...")
-    method.pay(amount)  // Dynamic polymorphism in action
+    method.pay(amount)
 }
 
 fun main() {
@@ -19,4 +18,10 @@ fun main() {
     println("\n=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
+
+    println("\n=== SMART HOME SYSTEM ===")
+    val lamp = SmartLamp(id = "L001", name = "Ruang Tamu")
+    val speaker = SmartSpeaker(id = "S001", name = "Google Nest Dapur")
+    val cctv = SmartCCTV(id = "C001", name = "Ezviz Garasi")
 }
+
