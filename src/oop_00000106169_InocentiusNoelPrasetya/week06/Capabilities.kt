32c9c8c29daa17b776f64b700fed51e94fcf51dd
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-03-24 23:24:21 +0700

    week06: create compound interfaces

---
+++ src/oop_00000106169_InocentiusNoelPrasetya/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000106169_InocentiusNoelPrasetya.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
