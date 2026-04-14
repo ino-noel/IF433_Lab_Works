9e274ddac89fa286b59bca902c4acc25aae1daaa
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-04-15 00:30:38 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000106169_InocentiusNoelPrasetya/week07/Main.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week07/Main.kt
@@ -6,19 +6,19 @@ fun main() {
     DatabaseManager.connect()
 
     println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()  // Instansiasi lewat Factory
+    val client = NetworkClient.createClient()
     client.connect()
 
     println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 22)
     val data2 = DataUser("Alice", 22)
-    println(data1)  // Otomatis readable format
-    println("Sama? ${data1 == data2}")  // True (Structural Equality)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 
     val data3 = data1.copy(age = 23)
     println("Hasil Copy: $data3")
 
-    val (userName, userAge) = data1  // Destructuring Declaration
+    val (userName, userAge) = data1
     println("Destructured: $userName berumur $userAge")
 
     println("\n=== TEST SEALED CLASS ===")
@@ -27,11 +27,17 @@ fun main() {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
-        ApiResponse.Loading -> "Tampilkan Spinner"  // Tambahkan ini
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
     println(uiMessage)
 
     println("\n=== GAME RPG SYSTEM ===")
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== RARITY & FACTORY ===")
+    println("Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata Awal: ${starterWeapon.item.name} - Damage: ${starterWeapon.item.damage} - Durability: ${starterWeapon.durability}")
 }
