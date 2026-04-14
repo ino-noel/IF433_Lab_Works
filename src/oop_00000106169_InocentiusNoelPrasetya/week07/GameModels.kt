6595045f9eb91933e04469f81576719e50b8db17
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-04-15 00:23:39 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000106169_InocentiusNoelPrasetya/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000106169_InocentiusNoelPrasetya.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(50),
+    RARE(30),
+    EPIC(10),
+    LEGENDARY(1)
+}
