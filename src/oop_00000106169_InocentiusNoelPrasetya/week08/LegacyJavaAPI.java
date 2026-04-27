f5e3a87db62f3ee479846fac650b6c2f969a60ef
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-04-27 20:31:37 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000106169_InocentiusNoelPrasetya/week08/LegacyJavaAPI.java
@@ -0,0 +1,9 @@
+package oop_00000106169_InocentiusNoelPrasetya.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya
+    // Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
