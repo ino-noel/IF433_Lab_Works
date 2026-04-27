0d1db014694806cfa6372f462f97edb702087745
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-04-27 20:35:38 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_00000106169_InocentiusNoelPrasetya/week08/ApiParser.kt
@@ -0,0 +1,38 @@
+package oop_00000106169_InocentiusNoelPrasetya.week08
+
+class ApiParser {
+
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) {
+            "API Invalid: Missing ID"
+        }
+
+        val name = requireNotNull(rawJson["name"] as? String) {
+            "API Invalid: Missing Name"
+        }
+
+        val type = rawJson["type"] as? String
+
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Product.Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All Size"
+                Product.Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+
+    fun checkout(product: Product) {
+        val productId = when (product) {
+            is Product.Electronic -> product.id
+            is Product.Clothing -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(productId)!!
+        println("[CHECKOUT SUCCESS] Transaction ID: $transactionId")
+    }
+}
