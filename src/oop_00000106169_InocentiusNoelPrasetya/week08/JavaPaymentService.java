050639d8b2c3d1d456231f4e1f59a9454bc72ad3
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-04-27 20:36:50 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000106169_InocentiusNoelPrasetya/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000106169_InocentiusNoelPrasetya.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
