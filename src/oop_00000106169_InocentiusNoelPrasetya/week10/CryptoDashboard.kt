e69c92d9c210076018dd267f048bf77814f415b2
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-05-05 20:22:46 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000106169_InocentiusNoelPrasetya/week10/CryptoDashboard.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week10/CryptoDashboard.kt
@@ -13,4 +13,14 @@ fun main() {
     response.data.forEach { coin ->
         println("Koin: ${coin.name} | Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 250.0))
+    txRepo.add(Transaction("TX002", -50.0))
+    txRepo.add(Transaction("TX003", 1000.0))
+
+    println("\n===== TRANSACTION HISTORY =====")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id} | Amount: ${tx.amount}")
+    }
 }
