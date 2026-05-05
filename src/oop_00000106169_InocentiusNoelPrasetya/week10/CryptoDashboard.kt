c99bcfce8460b3f5db512ba9dc770840792acabb
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-05-05 20:18:33 +0700

    week10: (task) populate coin repository

--- src/oop_00000106169_InocentiusNoelPrasetya/week10/CryptoDashboard.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000106169_InocentiusNoelPrasetya.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 3.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
