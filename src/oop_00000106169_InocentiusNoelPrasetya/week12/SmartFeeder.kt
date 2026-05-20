4c5a8c70e3f1a70f8e1c96f5c1803b94f0a3e329
Inocentius Noel Prasetya <inocentius.noel@gmail.com>
2026-05-20 09:33:27 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000106169_InocentiusNoelPrasetya/week12/SmartFeeder.kt
+++ src/oop_00000106169_InocentiusNoelPrasetya/week12/SmartFeeder.kt
@@ -34,4 +34,8 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    println("\n=== JADWAL MAKAN SORE ===")
+    runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
